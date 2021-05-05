07e98d45c0ef7a1ff0b15c0581df8598b1807132
DylanPiersanti <dylngae@outlook.com>
2021-05-05 04:11:37 +0200

    add Leaflet

--- src/components/Leaflet/index.js
+++ src/components/Leaflet/index.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component, useState } from "react";
+import { Text, View, StyleSheet, Switch } from "react-native";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 class Leaflet extends Component {
-	render() {
-		const { color, text } = this.props
+  render() {
+    const { color, text, mapsize } = this.props;
 
-		return (
-			<View style={styles.wrapper}>
-				<Text style={{ color }}>{text}</Text>
-			</View>
-		)
-	}
+	console.log(mapsize)
+
+    return (
+      <View style={{ position: "relative", height: 200 }}>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ width: "100%", height: "100%"}}
+        >
+          <TileLayer url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png" />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>{text}</Popup>
+          </Marker>
+        </MapContainer>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	wrapper: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}
-})
+  wrapper: {},
+});
 
-export default Leaflet
+export default Leaflet;
